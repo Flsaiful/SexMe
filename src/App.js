d6252c3a00ec94513ab0cde2c24901db94fb5c64
flsaiful <tusharimransaif@gmail.com>
2020-10-05 13:56:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { Link } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
 
 function App() {
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background-image">
+    <div  className="App">
+      <nav class="navbar navbar-light bg-light justify-content-center">
+       <h1 className="navbar-brand">Want To Free Sex With Me</h1>
+  
+    </nav>
+      <div className="style-bg">
+      <h2 className="p-4 text-white">Enter Information And Verify Email To Contact</h2>
+      <Tooltip title="My Detail Here" placement="right">
+            <Button className="bg-success text-white button-style">Click Here</Button>
+          </Tooltip>
+      </div>
+    </div>
     </div>
   );
 }
